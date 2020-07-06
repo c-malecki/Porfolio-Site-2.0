963725264f092f7e444a4b04f5be9ac84e2dfc67
Chris Malecki <chrismmalecki@gmail.com>
2020-07-05 23:39:17 -0400

    added LFG to projects. Not sure why CSS animation works differently live.

--- src/context/projectsData.js
+++ src/context/projectsData.js
@@ -1,5 +1,5 @@
 import Lod from "../assets/images/Lod.png";
-// import FTA from "../assets/images/FinancialTrackerApp.png";
+import LFG from "../assets/images/LFGwithicon.png";
 import Flo from "../assets/images/floswhistle.png";
 
 export const projects = [
@@ -25,19 +25,17 @@ export const projects = [
     github: "https://github.com/c-malecki/pod-character-calculator",
     demo: "https://diabloii-cc.netlify.app/",
   },
-  // {
-  //   pic: FTA,
-  //   alt: "Financial Tracker Application",
-  //   title: "Financial Tracker App",
-  //   titleId: 2,
-  //   description:
-  //     "Personal finance tracker similar to banking app. Uses React, React-Router, Redux, Formik, Yup, Bootstrap.",
-  //   descriptionId: 2,
-  //   github: "https://github.com/c-malecki/FinancialTrackerApp",
-  //   demo: "https://codesandbox.io/s/github/c-malecki/FinancialTrackerApp",
-  // },
+  {
+    pic: LFG,
+    alt: "LFG social media app",
+    title: "LFG - Looking for Group",
+    titleId: 2,
+    description: "Social media site for connecting gamers with play groups.",
+    descriptionId: 2,
+    github: "https://github.com/c-malecki/lfg-app",
+    demo: "https://lfg-demo.netlify.app/",
+  },
 ];
-
 
 // {
 //   pic: FTA,
