e7800cf3902d13dcf59117d942be2e53a90d1755
Chris Malecki <chrismmalecki@gmail.com>
2020-07-24 16:37:08 -0400

    header changes for blog page + added twitter icon/link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,6 +18,13 @@ export const Footer = () => {
         >
           <i className="fab fa-github-square" aria-hidden="true" />
         </a>
+        <a
+          href="https://twitter.com/ChrisMalecki1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-twitter-square" aria-hidden="true" />
+        </a>
       </div>
       <span>Chris Malecki ©2020</span>
     </div>
